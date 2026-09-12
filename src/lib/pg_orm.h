c794529db8d8e70040b82724237330adc7c2dc6a
Thiago Kasper de Souza <164453378+ThiagoKasperSouza@users.noreply.github.com>
2026-09-12 08:36:14 -0300

    Update pg_orm.h

--- src/lib/pg_orm.h
+++ src/lib/pg_orm.h
@@ -14,6 +14,6 @@ void create_table(PGconn *conn, char* sql);
 PGresult* exec_read_list(PGconn *conn,char *cols, char *table);
 PGresult* exec_insert(PGconn *conn, const char *table, const char *cols, int n_params, const char * const *param_values);
 PGresult* exec_single_read(PGconn *conn,char *cols, char *table, char *where);
-PGresult* exec_update(PGconn *conn, const char *table, const char *set_clause, const char *where_clause, int n_params, const char * const *param_values);
-PGresult* exec_delete(PGconn *conn, const char *table, const char *where_clause, int n_params, const char * const *param_values);
+PGresult* exec_update(PGconn *conn, char *sql, int n_params, const char * const *param_values);
+PGresult* exec_delete(PGconn *conn, char *sql, int n_params, const char * const *param_values);
 #endif
